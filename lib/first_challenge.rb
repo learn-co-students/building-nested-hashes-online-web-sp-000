bd2fa179ec1ff4cc5109b17a1103836ef36d7a7f
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-12-23 00:27:10 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,60 @@
 def first_challenge
-   epic_tragedy = {}
+  epic_tragedy = {
+    montague:{},
+    capulet:{}
+  }
+=begin
+   epic_tragedy = {
+     montague:{
+       patriarch:{
+         name:"Lord Montague",
+         Age:53
+       },
+       matriarch:{
+         name:"Lady Montague",
+         Age:54
+       },
+       hero:{
+         name:"Romeo",
+         age:15,
+         status:"alive"
+       },
+       hero_friends:[
+         {
+            :name => "Benvolio",
+            :age => "17",
+            :attitude => "worried"
+         },
+         {
+           :name => "Mercutio",
+           :age => "18",
+           :attitude => "hot-headed"
+         }
+       ]
+     },
+     capulet:{
+       patriarch:{
+         name:"Lord Capulet",
+         age:50
+       },
+       matriarch:{
+         name:"Lady Capulet",
+         age:51
+       },
+       heroine:{
+         name:"Juliet",
+         age:15,
+         status:"alive"
+       },
+       heroine_friends:[
+        {name:"Steven",
+          age:30,
+          attitude:"confused"},
+        {name:"Nurse",
+          age:44,
+          attitude:"worried"}
+       ]
+     }
+   }
+=end
 end
