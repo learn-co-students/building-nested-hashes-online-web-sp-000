5d6c0aa288696240e5dc97eda8feff34425ee2d6
Ismail Jalloh <ijalloh21@gmail.com>
2018-12-30 12:04:36 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,7 +4,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
+      :hero_friends => [{:age=>"17", :attitude=>"worried", :name=>"Benvolio"}, {:age=>"18", :attitude=>"hot-headed", :name=>"Mercutio"}
         #your hashes here!
       ]
    }, 
@@ -12,7 +12,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+      :heroine_friends => [{:age=>"30", :attitude=>"confused", :name=>"Steven"}, {:age=>"44", :attitude=>"worried", :name=>"Nurse"}
         #your hashes here!
       ]
    }
