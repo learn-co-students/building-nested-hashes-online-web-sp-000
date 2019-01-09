1443f8bd649122f3bb6f3290a5da29be5ff5ce28
Dr. XyClo <sdcarmody@gmail.com>
2019-01-09 02:01:29 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,17 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+        {
+          age: "17",
+          attitude: "worried",
+          name: "Benvolio"
+          
+        },
+        {
+          age: "18",
+          attitude: "hot-headed",
+          name:"Mercutio"
+        }
       ]
    }, 
    :capulet => {
@@ -13,7 +23,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+          {
+          age: "30",
+          attitude: "confused",
+          name: "Steven"
+        },
+        {
+          age: "44",
+          attitude: "worried",
+          name:"Nurse"
+        }
       ]
    }
 }
