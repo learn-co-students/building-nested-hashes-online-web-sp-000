564f5057f621b050375c4b53854644aff9d545ab
Bernadette Davis <bsd231@nyu.edu>
2019-07-05 14:05:14 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,21 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+    montague: {
+      patriarch: {},
+      matriarch: {},
+      hero: {},
+      hero_friends: []
       #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+      },
 
-  
+    capulet: {
+      patriarch: {},
+      matriarch: {},
+      heroine: {},
+      heroine_friends: []
+      #your key/value pairs here!
+      }
+    }
+
+
 end
