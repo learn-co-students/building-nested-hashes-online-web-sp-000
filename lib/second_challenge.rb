4a79945e6be13ad3b392218892fd5708b7010807
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-22 01:15:17 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,43 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+       :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
+        {
+         :name => "Benvolio",
+         :age => "17",
+         :attitude => "worried"
+      },
+      {
+        :name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"
+      }
+      ]
+      
       #your key/value pairs here!
+      
       }, 
     :capulet => {
+        :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+        {
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+      },
+      {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
+      }
+      ]
+   }
+   }
       #your key/value pairs here!
       }}
 
