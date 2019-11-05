f8e4e27d019c4f224032641751e00677de18889e
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-05 02:29:45 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,9 +21,13 @@ def bonus
   }
 
   #code your solution here:
-
+  ob = epic_tragedy
+  ob[:montague][:hero][:status] = 'dead'
+  ob[:capulet][:heroine][:status] = 'dead'
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
+
+bonus
