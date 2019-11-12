c6998709182d5b4d1db8b9a1aab3e7833004f8a8
Nicole Rothenay <nthorn1211@gmail.com>
2019-11-12 23:23:03 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,6 +6,10 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        {name: "Benvolio", age: "17", attitude: "worried"
+        },
+        {name: "Mercutio", age: "15", attitude: "hot-headed"
+        }
       ]
    }, 
    :capulet => {
@@ -14,6 +18,12 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {
+          name: "Steven", age: "30", attitude: "confused"
+        }
+        {
+          name: "Nurse", age: "44", attitude: "worried"
+        }
       ]
    }
 }
