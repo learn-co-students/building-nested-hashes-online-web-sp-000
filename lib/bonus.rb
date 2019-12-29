7d289f2a5f505bae4083adf3886e247586e5a6de
Mia <tamiapaschall@gmail.com>
2019-12-29 01:19:06 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,5 +1,5 @@
 def bonus
-  epic_tragedy = {
+   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,18 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+       :patriarch => {:name => "Lord Montague", :age => "53", },
+       :matriarch => {:name => "Lady Montague", :age => "54", },
+       :hero => {:name => "Romeo", :age => "15", :status => "alive" },
+       :hero_friends => [],
+     :capulet => {
+       :patriarch => {:name => "Lord Capulet", :age => "50", },
+       :matriarch => {:name => "Lady Montague", :age => "51", },
+       :heroine => {:name => "Juliet", :age => "15", :status => "alive" },
+       :heroine_friends => [
+         {:name => "Steve", :age => "30", :status => "confused"},
+         {:name => "Nurse", :age => "44", :status => "worried"}
+         ]
+     }
+   }
 end
