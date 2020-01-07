6f2f981c30c5df91c056bfe4a35a3f4866998f63
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-07 20:16:41 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,35 +1,18 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        name: "Lord Montague", age: "53"
-        },
-      :matriarch => {
-        #your key/value pairs here
-        name: "Lady Montague", age: "54"
-        },
-      :hero => {
-        #your key/value pairs here
-        name: "Romeo", age: "15", status: "alive"
-        },
-      :hero_friends => [{:age=>"17", :attitude=>"worried", :name=>"Benvolio"}, {:age=>"18", :attitude=>"hot-headed", :name=>"Mercutio"}]
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        name: "Lord Capulet", age: "50"
-        },
-      :matriarch => {
-        #your key/value pairs here
-        name: "Lady Capulet", age: "51"
-        },
-      :heroine => {
-        #your key/value pairs here
-        name: "Juliet", age: "15", status: "alive"
-        },
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
    }
   }
 
   
+end
