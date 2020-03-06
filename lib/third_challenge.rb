49281d76763c1422daad1d45e975d44fab5d8906
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-03-06 16:28:59 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,26 +1,22 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        #your key/value pairs here
+      :patriarch => {:name => "Lord Montague", :age => "53"
+       },
+      :matriarch => { :name => "Lady Montague", :age => "54"
         },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
+      :hero => {:name =>"Romeo", :age => "15", :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        #your key/value pairs here
+      :patriarch => { :name => "Lord Capulet", :age => "50"
         },
-      :matriarch => {
-        #your key/value pairs here
+      :matriarch => {:name => "Lady Capulet", :age => "51"
+      
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet", :age => "15", :status => "alive"
         },
       :heroine_friends => []
    }
