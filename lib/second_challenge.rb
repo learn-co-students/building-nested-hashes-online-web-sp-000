7130a4e469408630c30c1518c6ef4d9d44fedf38
Monica Gerard <mlgerardvla@gmail.com>
2020-03-15 19:56:17 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
-      }, 
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []  #your key/value pairs here!
+    },
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+    }
+  }
 
-  
+
 end
