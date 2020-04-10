81b4f9122817f920b28d2bda679141364a820570
Jack Cooper <jackcooper1245@gmail.com>
2020-04-10 12:05:37 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,9 +20,7 @@ def bonus
    }
   }
 
-  #code your solution here:
-
-  
+epic_tragedy.has_key?()
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
