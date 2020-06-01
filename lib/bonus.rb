d4398205b7e425958114e56cca5f0e7127980c97
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-01 18:27:51 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   epic_tragedy[:montague][:hero][:status] = "dead"
-  #epic_tragedy[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 
   epic_tragedy
