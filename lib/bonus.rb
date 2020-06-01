87ac353d61a1959339dc24dc4b6f61e0bc067506
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-01 18:09:44 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,6 +24,5 @@ def bonus
   epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
