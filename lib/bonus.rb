9d0ddb072a02f4a155de2912563f627316ac90a5
reevesalic <reevesalic@gmail.com>
2020-06-19 23:17:45 -0400

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -25,5 +25,5 @@ def bonus
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  epic_tragedy
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 end
