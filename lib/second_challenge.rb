cd158ec5ec621967e72365983f09af99caa1f8ef
jasonronalddavis <jdavis@c.ringling.edu>
2020-07-20 18:02:34 -0500

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,19 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+    montague:  {
+      patriarch: {},
+      matriarch: {},
+      hero: {},
+      hero_friends: []
       #your key/value pairs here!
       }, 
-    :capulet => {
+    capulet: {
       #your key/value pairs here!
-      }}
-
-  
+      patriarch: {},
+      matriarch: {},
+      heroine: {},
+      heroine_friends: []
+      #your key/value pairs here! 
+      }
+    }
 end
