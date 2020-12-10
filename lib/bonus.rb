2f0aafb6736ba3e6c87e84b8fdc74559d0c80a95
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-10 17:23:06 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,9 +20,7 @@ def bonus
    }
   }
   
-  # epic_tragedy[:hero][:status] = "dead"
-  # epic_tragedy[:heroine][:status] = "dead"
-  
+ 
 epic_tragedy[:montague][:hero][:status] = "dead"
 epic_tragedy[:capulet][:heroine][:status] = "dead"
 
