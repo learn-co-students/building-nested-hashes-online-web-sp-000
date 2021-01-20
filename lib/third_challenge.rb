9f7c9690e1cb430f2bed00ce3fb3b9c40e84d926
Jessica Natovich <jessicanatovich@yahoo.com>
2021-01-20 23:34:21 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,30 +1,13 @@
 def third_challenge
+ 
+
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
-   }, 
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
+      :patriarch => {name: "Lord Montague", age: "53"},
+
+      :heroine => {name: "Juliette", age: "15", status: "alive"},
       :heroine_friends => []
    }
+}
   }
-
-  
-end
+end 
