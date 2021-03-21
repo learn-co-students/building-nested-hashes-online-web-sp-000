40c182c7987627963d664b4361a64f9ff94b6b5a
Harrison Price <hprice518@gmail.com>
2021-03-21 19:01:32 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,16 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+       :patriarch => {
+         :name => "Lord Montague",
+         :age => 53
+          },
+        :matriarch => {
+          :name => "Lady Montague"
+          :age => "54"
+        }
+       }
+     :capulet => {
+     }
+   }
 end
