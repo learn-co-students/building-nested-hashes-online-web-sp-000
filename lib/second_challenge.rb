4ea982c91b84aa0b18cee2429efb54b56fce464d
Harrison Price <hprice518@gmail.com>
2021-03-21 19:25:35 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,13 +4,13 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => {},
+      :hero_friends => [],
       }, 
     :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => {},
+      :heroine_friends => [],
       }
     
   }
