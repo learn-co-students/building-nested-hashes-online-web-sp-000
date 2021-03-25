9acfabd610f3e1d05cd4397f38ffe295b8637eb7
Harrison Price <hprice518@gmail.com>
2021-03-25 02:51:58 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,37 +2,36 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague"
+        :name => "Lord Montague",
         :age => "53"
         },
       :matriarch => {
-        :name => "Lady Montague"
+        :name => "Lady Montague",
         :age => "54"
         },
       :hero => {
-       :name => "Romeo"
-        :age => "15"
+       :name => "Romeo",
+        :age => "15",
         :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        :name => "Lord Capulet"
+        :name => "Lord Capulet",
         :age => "50"
         },
       :matriarch => {
-        :name => "Lady Capulet"
+        :name => "Lady Capulet",
         :age => "51"
         },
       :heroine => {
-        :name => "Juliet"
-        :age => "15"
+        :name => "Juliet",
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => []
-   }
+    }
   }
-
   
 end
