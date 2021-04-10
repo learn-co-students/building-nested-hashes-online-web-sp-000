2d5ef419dae8d1d3d134e3f8b08abfdc63d523a6
danpaslawsky <danpaslawsky@gmail.com>
2021-04-10 11:51:42 -0700

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,11 +1,14 @@
 def fourth_challenge
   epic_tragedy = {
-   :montague => {
+
+    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        {:name => "Benvolio", :age => "17", :attitude => "worried"}, {:name => "Mercutio",
+        :age => "18", :attitude => "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -14,10 +17,15 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {:name => "Steven", :age => "30", :attitude => "confused"}, {:name => "Nurse",
+        :age => "44", :attitude => "worried"}
       ]
-   }
+    }
+    
 }
-
-  
-
+    
 end
+
+
+#epic_tragedy[:montague][:hero][:status] = "dead"
+#epic_tragedy[:capulet][:heroine][:status] = "dead"
